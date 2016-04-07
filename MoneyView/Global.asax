607a5abd0c07b9d8c3c2f0a389d4f97b1d6b14d7
ROman-Ryabko <ryabko.work@gmail.com>
2016-04-07 08:03:03 +0300

    add mvc project

---
+++ MoneyView/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoneyView.MvcApplication" Language="C#" %>
---
+++ MoneyView/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoneyView.Startup))]
+namespace MoneyView
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
